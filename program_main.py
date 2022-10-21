9f3f7a4860890b550e5423c403317c7108815ee4
shamin daniil <danya.shamin.06@mail.ru>
2022-10-21 17:48:36 +0300

    delete all PyQt5 code and add sreen.get_buffer()

--- program_main.py
+++ program_main.py
@@ -1,24 +1,8 @@
 import sys
-from PyQt5.QtWidgets import QWidget, QApplication
-from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtGui import QImage, QPainter
+import pygame as pg
+pg.init()
 
-class Example(QWidget):
-    def __init__(self, image_name):
-        super().__init__()
-        self.image_name = image_name
-        self.initGui()
-    def initGui(self):
-        self.setGeometry(300, 300, 300, 220)
-        self.setWindowTitle('Icon')
-        self.setWindowIcon(QIcon(icons[self.image_name]))
-        self.show()
-
-icons = {'my_icon_1':'images/my_icon_1.bmp', 'my_icon_2':'images/my_icon_2.bmp'}
-
-if __name__ == '__main__':
-    app_1 = QApplication(sys.argv)
-    ex = Example('my_icon_1')
-    app_2 = QApplication(sys.argv)
-    ex_2 = Example('my_icon_2')
-    sys.exit(app_1.exec_())
-    sys.exit(app_2.exec_())
+screen = pg.display.set_mode((20, 20))
+print(screen.get_buffer())
