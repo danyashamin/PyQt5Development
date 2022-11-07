497635c646e8cf7848db4cc8b4f3f349982a01ed
shamin daniil <danya.shamin.06@mail.ru>
2022-11-07 20:17:55 +0300

    try unseccessfully use pygame with PyQt5

--- program_main.py
+++ program_main.py
@@ -1,8 +1,6 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout
 from PyQt5.QtGui import QImage, QPainter
 import pygame as pg
 pg.init()
 
-screen = pg.display.set_mode((20, 20))
-print(screen.get_buffer())
