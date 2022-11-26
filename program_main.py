bfb951d2902b05d4224349c093926c6a0dc1c396
shamin daniil <danya.shamin.06@mail.ru>
2022-11-26 08:48:55 +0300

    unseccessfully try add surface pygame in widget

--- program_main.py
+++ program_main.py
@@ -1,6 +1,19 @@
+from PyQt5.QtWidgets import QApplication, QWidget, QHBoxLayout, QVBoxLayout, QLabel, QRadioButton, QGroupBox, QButtonGroup
+from PyQt5.QtCore import Qt
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout
-from PyQt5.QtGui import QImage, QPainter
-import pygame as pg
-pg.init()
 
+class MainWindow(QWidget):
+    def __init__(self, question, *args):
+        super().__init__()
+        self.common_line = QVBoxLayout()
+        self.question_label = QLabel(self)
+        self.question_label.setText(question)
+        self.question_line = QHBoxLayout()
+        self.question_line.addWidget(self.question_label, alignment=Qt.AlignTop)
+        self.common_line.addLayout(self.question_line)
+        self.setLayout(self.common_line)
+        self.show()
+        sys.exit(app.exec_())
+
+app = QApplication(sys.argv)
+m = MainWindow('Сколько капель в море?')
