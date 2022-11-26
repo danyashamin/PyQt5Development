79b9aa56bb70625446beea20b7a342ea0adbdf84
shamin daniil <danya.shamin.06@mail.ru>
2022-11-26 17:39:39 +0300

    practise on PyQt5

--- program_main.py
+++ program_main.py
@@ -1,19 +1,21 @@
-from PyQt5.QtWidgets import QApplication, QWidget, QHBoxLayout, QVBoxLayout, QLabel, QRadioButton, QGroupBox, QButtonGroup
+from PyQt5.QtWidgets import QApplication, QWidget, QHBoxLayout, QVBoxLayout, QLabel, QRadioButton, QGroupBox, QButtonGroup, QPushButton
 from PyQt5.QtCore import Qt
 import sys
 
-class MainWindow(QWidget):
-    def __init__(self, question, *args):
+class TestWind(QWidget):
+    def __init__(self, questions):
         super().__init__()
-        self.common_line = QVBoxLayout()
-        self.question_label = QLabel(self)
-        self.question_label.setText(question)
-        self.question_line = QHBoxLayout()
-        self.question_line.addWidget(self.question_label, alignment=Qt.AlignTop)
-        self.common_line.addLayout(self.question_line)
-        self.setLayout(self.common_line)
+        self.setGeometry(300, 300, 500, 300)
         self.show()
-        sys.exit(app.exec_())
+        global cur_question_number
+        self.line_box = QHBoxLayout()
+        self.box_answers = QGroupBox('Варианты ответов:')
+        self.box_line_1 = QHBoxLayout()
+        self.answer_1 = QRadioButton(questions[cur_question_number])
+        if cur_question_number==0:
+            sys.exit(app.exec_())
 
 app = QApplication(sys.argv)
-m = MainWindow('Сколько капель в море?')
+questions = [['Что обычно капает с неба?', 'Капли дождя', 'Деньги','Цинк', 'Ртуть']]
+cur_question_number = 0
+win = TestWind(questions)
